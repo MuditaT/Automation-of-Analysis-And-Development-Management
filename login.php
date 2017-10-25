541dd5659c60a0084247220458e3a0274d2d696a
Mudita Tandon <mudita.tandon94@gmail.com>
2017-10-25 12:17:48 -0400

    Update login.php

--- login.php
+++ login.php
@@ -10,13 +10,13 @@ else
 ?>
 <html>
 <head>
-<title> SDLC PARTNERS</title>
+<title> SDLC</title>
 <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body id="full">
 <div id="head">
 <div id="head_cen">
-<div id="logo"><a href="main.php"><img src="images.jpg" alt="SDLC PARTNERS LOGO" style="width:160px;height:80px"></a><p><center>"YOUR VISSION OUR FUTURE"<center></p>
+<div id="logo"><a href="main.php"><img src="images.jpg" alt="SDLC LOGO" style="width:160px;height:80px"></a><p><center>"YOUR VISSION OUR FUTURE"<center></p>
 </div>
 <div id="login">
 <?php
